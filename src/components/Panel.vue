a9ba07fe1246027a169f01c3ee0eeabd49d9719f
Will Freeman <hohosanta@me.com>
2022-06-23 18:57:24 -0600

    first

---
+++ src/components/Panel.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="panel">
+    <img @click="close" class="button" src="../assets/slice1.svg" alt="">
+    <img @click="open" class="button" src="../assets/slice2.svg" alt="">
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Panel",
+  methods: {
+    open() {
+      axios.get("/open");
+    },
+    close() {
+      axios.get("/close");
+    }
+  }
+}
+</script>
+
+<style scoped>
+.panel {
+  background-color: #929292;
+  margin: 20px 20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.button {
+  height: 200px;
+  width: 200px;
+  margin-top: 35px;
+  margin-bottom: 35px;
+}
+</style>
